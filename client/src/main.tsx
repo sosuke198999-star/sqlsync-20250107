25691989989b0251beb88f567f3de61662385ab9
sosuke198999-star <sosuke198999@gmail.com>
2025-10-31 17:17:18 +0700

    Fix: use alias import for i18n to avoid resolution errors in Docker builds

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./lib/i18n";
+// Use path alias to ensure resolution in case-sensitive filesystems during CI/Docker builds
+import "@/lib/i18n";
 
 createRoot(document.getElementById("root")!).render(<App />);
